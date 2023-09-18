e50aab7e4517c92d4936c0dda5a2292522eb5442
Ålexander <yurchenko@post.com>
2023-09-18 02:23:32 +0200

    Made hamburger Nav / made Skills a bit prettier

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,15 +1,40 @@
 // NextJS components:
 import Image from "next/image";
 import Link from "next/link";
+// React components:
+import { useState } from "react";
 // CSS:
 import styles from "../styles/Navbar.module.css";
 // Data:
 import { navItems } from "../data/data";
 
 export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
   return (
     <nav className={styles.nav}>
-      <ul className={styles.ul}>
+      <ul className={styles.wideScreen}>
+        {navItems.map(({ id, img, alt, url }) => (
+          <li className={styles.li} key={id}>
+            <Link href={url}>
+              <Image src={img} alt={alt} width={300} height={35} />
+            </Link>
+          </li>
+        ))}
+      </ul>
+
+      <div className={styles.hamburgerMenu} onClick={toggleMenu}>
+        <div className={styles.bar}></div>
+        <div className={styles.bar}></div>
+        <div className={styles.bar}></div>
+      </div>
+      <ul
+        className={`${styles.menuItems} ${isOpen ? styles.open : ""}`}
+        onClick={toggleMenu}
+      >
         {navItems.map(({ id, img, alt, url }) => (
           <li className={styles.li} key={id}>
             <Link href={url}>
